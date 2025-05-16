8ab1b4a924ea6d606626a3890ebc31c705996f25
Ram Sthapit <ramsthapit50@gmail.com>
2025-05-16 14:04:44 +0545

    feat(): api integrated

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -24,7 +24,7 @@ const Pagination = ({ currentPage, setCurrentPage, totalPage,itemNo }) => {
           <li className={`z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white`}>
             {currentPage}
           </li>
-          <li className={`${currentPage >= parseInt(totalPage/10) && "hidden"} flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}>
+          <li className={`${currentPage >= totalPage/10 && "hidden"} flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}>
             <FaAngleRight onClick={paginateFront}/>
           </li>
         </ul>
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,10 +3,15 @@ import { FaEdit, FaTrash } from 'react-icons/fa'
 import Pagination from '../components/Pagination'
 // https://dummyjson.com/docs/products#products-limit_skip
 // https://dummyjson.com/products?limit=10&skip=20&select=title,category,rating,price
+
 const Dashboard = () => {
   
   const [currentPage, setCurrentPage] = useState(1)
   const [itemNo, setItemNo] = useState(1)
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [totalPage, setTotalPage] = useState(0)
+
   const Category = [
     { id: 1, name: 'S.N.' },
     { id: 1, name: 'Product name' },
@@ -16,86 +21,99 @@ const Dashboard = () => {
     { id: 5, name: '' },
   ]
 
-  const Data = {
-    Products: [
-      {
-        "id": 21,
-        "title": "Cucumber",
-        "category": "groceries",
-        "rating": 4.07,
-        "price": 1.49
-      },
-      {
-        "id": 22,
-        "title": "Dog Food",
-        "category": "groceries",
-        "rating": 4.55,
-        "price": 10.99
-      },
-      {
-        "id": 23,
-        "title": "Eggs",
-        "category": "groceries",
-        "rating": 2.53,
-        "price": 2.99
-      },
-      {
-        "id": 24,
-        "title": "Fish Steak",
-        "category": "groceries",
-        "rating": 3.78,
-        "price": 14.99
-      },
-      {
-        "id": 25,
-        "title": "Green Bell Pepper",
-        "category": "groceries",
-        "rating": 3.25,
-        "price": 1.29
-      },
-      {
-        "id": 26,
-        "title": "Green Chili Pepper",
-        "category": "groceries",
-        "rating": 3.66,
-        "price": 0.99
-      },
-      {
-        "id": 27,
-        "title": "Honey Jar",
-        "category": "groceries",
-        "rating": 3.97,
-        "price": 6.99
-      },
-      {
-        "id": 28,
-        "title": "Ice Cream",
-        "category": "groceries",
-        "rating": 3.39,
-        "price": 5.49
-      },
-      {
-        "id": 29,
-        "title": "Juice",
-        "category": "groceries",
-        "rating": 3.94,
-        "price": 3.99
-      },
-      {
-        "id": 30,
-        "title": "Kiwi",
-        "category": "groceries",
-        "rating": 4.93,
-        "price": 2.49
-      }
-    ],
-    "total": 194,
-    "skip": 20,
-    "limit": 10
-  }
+  // const Data = {
+  //   Products: [
+  //     {
+  //       "id": 21,
+  //       "title": "Cucumber",
+  //       "category": "groceries",
+  //       "rating": 4.07,
+  //       "price": 1.49
+  //     },
+  //     {
+  //       "id": 22,
+  //       "title": "Dog Food",
+  //       "category": "groceries",
+  //       "rating": 4.55,
+  //       "price": 10.99
+  //     },
+  //     {
+  //       "id": 23,
+  //       "title": "Eggs",
+  //       "category": "groceries",
+  //       "rating": 2.53,
+  //       "price": 2.99
+  //     },
+  //     {
+  //       "id": 24,
+  //       "title": "Fish Steak",
+  //       "category": "groceries",
+  //       "rating": 3.78,
+  //       "price": 14.99
+  //     },
+  //     {
+  //       "id": 25,
+  //       "title": "Green Bell Pepper",
+  //       "category": "groceries",
+  //       "rating": 3.25,
+  //       "price": 1.29
+  //     },
+  //     {
+  //       "id": 26,
+  //       "title": "Green Chili Pepper",
+  //       "category": "groceries",
+  //       "rating": 3.66,
+  //       "price": 0.99
+  //     },
+  //     {
+  //       "id": 27,
+  //       "title": "Honey Jar",
+  //       "category": "groceries",
+  //       "rating": 3.97,
+  //       "price": 6.99
+  //     },
+  //     {
+  //       "id": 28,
+  //       "title": "Ice Cream",
+  //       "category": "groceries",
+  //       "rating": 3.39,
+  //       "price": 5.49
+  //     },
+  //     {
+  //       "id": 29,
+  //       "title": "Juice",
+  //       "category": "groceries",
+  //       "rating": 3.94,
+  //       "price": 3.99
+  //     },
+  //     {
+  //       "id": 30,
+  //       "title": "Kiwi",
+  //       "category": "groceries",
+  //       "rating": 4.93,
+  //       "price": 2.49
+  //     }
+  //   ],
+  //   "total": 194,
+  //   "skip": 20,
+  //   "limit": 10
+  // }
   useEffect(() => { 
-    setItemNo(Data.Products[0].id)
+    fetch(`https://dummyjson.com/products?limit=10&skip=${(currentPage-1)*10}&select=title,category,rating,price`)
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data.products);
+        setItemNo(data.products[0].id)
+        setTotalPage(data.total);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error('Error fetching data:', err);
+        setLoading(false);
+      });
+    
   }, [currentPage])
+  
 
   return (
     <main className="flex w-full h-full bg-gray-100 p-5"> 
@@ -105,16 +123,16 @@ const Dashboard = () => {
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
-                    {Category.map((item) => (
-                      <th key={item.id} scope="col" className="px-3 py-3 text-gray-500">
+                    {Category.map((item, index) => (
+                      <th key={index} scope="col" className="px-3 py-3 text-gray-500">
                         {item.name}
                       </th>))
                     }
                   </tr>
                 </thead>
                 <tbody>
-                  {Data.Products.map((item) => (
-                    <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">  
+                  {products.map((item, index) => (
+                    <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">  
                       <td className="pl-6">
                           {item.id}
                       </td>
@@ -145,7 +163,7 @@ const Dashboard = () => {
           <Pagination
             currentPage={currentPage}
             setCurrentPage={setCurrentPage}
-            totalPage={Data.total}
+            totalPage={totalPage}
             itemNo={itemNo}
           />
       </div>
