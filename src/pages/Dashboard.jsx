444fa03cdcc1a36685e571b4db20006b04519eea
Ram Sthapit <ramsthapit50@gmail.com>
2025-05-16 20:42:44 +0545

    feat(dashboard): category data fetch

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,6 +14,7 @@ const Dashboard = () => {
   const [totalItems, setTotalItems] = useState(0)
   const [selectedCategory, setSelectedCategory] = useState("")
   const [error, setError] = useState({})
+  const [categories, setCategories] = useState([])
 
   const TableHeading = [
     { id: 1, name: 'S.N.' },
@@ -24,128 +25,128 @@ const Dashboard = () => {
     { id: 5, name: '' },
   ]
 
-  const categories = [
-    {
-        "slug": "beuty",
-        "name": "Beauty",
-        "url": "https://dummyjson.com/products/category/beauty"
-    },
-    {
-        "slug": "fragrances",
-        "name": "Fragrances",
-        "url": "https://dummyjson.com/products/category/fragrances"
-    },
-    {
-        "slug": "furniture",
-        "name": "Furniture",
-        "url": "https://dummyjson.com/products/category/furniture"
-    },
-    {
-        "slug": "groceries",
-        "name": "Groceries",
-        "url": "https://dummyjson.com/products/category/groceries"
-    },
-    {
-        "slug": "home-decoration",
-        "name": "Home Decoration",
-        "url": "https://dummyjson.com/products/category/home-decoration"
-    },
-    {
-        "slug": "kitchen-accessories",
-        "name": "Kitchen Accessories",
-        "url": "https://dummyjson.com/products/category/kitchen-accessories"
-    },
-    {
-        "slug": "laptops",
-        "name": "Laptops",
-        "url": "https://dummyjson.com/products/category/laptops"
-    },
-    {
-        "slug": "mens-shirts",
-        "name": "Mens Shirts",
-        "url": "https://dummyjson.com/products/category/mens-shirts"
-    },
-    {
-        "slug": "mens-shoes",
-        "name": "Mens Shoes",
-        "url": "https://dummyjson.com/products/category/mens-shoes"
-    },
-    {
-        "slug": "mens-watches",
-        "name": "Mens Watches",
-        "url": "https://dummyjson.com/products/category/mens-watches"
-    },
-    {
-        "slug": "mobile-accessories",
-        "name": "Mobile Accessories",
-        "url": "https://dummyjson.com/products/category/mobile-accessories"
-    },
-    {
-        "slug": "motorcycle",
-        "name": "Motorcycle",
-        "url": "https://dummyjson.com/products/category/motorcycle"
-    },
-    {
-        "slug": "skin-care",
-        "name": "Skin Care",
-        "url": "https://dummyjson.com/products/category/skin-care"
-    },
-    {
-        "slug": "smartphones",
-        "name": "Smartphones",
-        "url": "https://dummyjson.com/products/category/smartphones"
-    },
-    {
-        "slug": "sports-accessories",
-        "name": "Sports Accessories",
-        "url": "https://dummyjson.com/products/category/sports-accessories"
-    },
-    {
-        "slug": "sunglasses",
-        "name": "Sunglasses",
-        "url": "https://dummyjson.com/products/category/sunglasses"
-    },
-    {
-        "slug": "tablets",
-        "name": "Tablets",
-        "url": "https://dummyjson.com/products/category/tablets"
-    },
-    {
-        "slug": "tops",
-        "name": "Tops",
-        "url": "https://dummyjson.com/products/category/tops"
-    },
-    {
-        "slug": "vehicle",
-        "name": "Vehicle",
-        "url": "https://dummyjson.com/products/category/vehicle"
-    },
-    {
-        "slug": "womens-bags",
-        "name": "Womens Bags",
-        "url": "https://dummyjson.com/products/category/womens-bags"
-    },
-    {
-        "slug": "womens-dresses",
-        "name": "Womens Dresses",
-        "url": "https://dummyjson.com/products/category/womens-dresses"
-    },
-    {
-        "slug": "womens-jewellery",
-        "name": "Womens Jewellery",
-        "url": "https://dummyjson.com/products/category/womens-jewellery"
-    },
-    {
-        "slug": "womens-shoes",
-        "name": "Womens Shoes",
-        "url": "https://dummyjson.com/products/category/womens-shoes"
-    },
-    {
-        "slug": "womens-watches",
-        "name": "Womens Watches",
-        "url": "https://dummyjson.com/products/category/womens-watches"
-    }
-]
+//   const categories = [
+//     {
+//         "slug": "beuty",
+//         "name": "Beauty",
+//         "url": "https://dummyjson.com/products/category/beauty"
+//     },
+//     {
+//         "slug": "fragrances",
+//         "name": "Fragrances",
+//         "url": "https://dummyjson.com/products/category/fragrances"
+//     },
+//     {
+//         "slug": "furniture",
+//         "name": "Furniture",
+//         "url": "https://dummyjson.com/products/category/furniture"
+//     },
+//     {
+//         "slug": "groceries",
+//         "name": "Groceries",
+//         "url": "https://dummyjson.com/products/category/groceries"
+//     },
+//     {
+//         "slug": "home-decoration",
+//         "name": "Home Decoration",
+//         "url": "https://dummyjson.com/products/category/home-decoration"
+//     },
+//     {
+//         "slug": "kitchen-accessories",
+//         "name": "Kitchen Accessories",
+//         "url": "https://dummyjson.com/products/category/kitchen-accessories"
+//     },
+//     {
+//         "slug": "laptops",
+//         "name": "Laptops",
+//         "url": "https://dummyjson.com/products/category/laptops"
+//     },
+//     {
+//         "slug": "mens-shirts",
+//         "name": "Mens Shirts",
+//         "url": "https://dummyjson.com/products/category/mens-shirts"
+//     },
+//     {
+//         "slug": "mens-shoes",
+//         "name": "Mens Shoes",
+//         "url": "https://dummyjson.com/products/category/mens-shoes"
+//     },
+//     {
+//         "slug": "mens-watches",
+//         "name": "Mens Watches",
+//         "url": "https://dummyjson.com/products/category/mens-watches"
+//     },
+//     {
+//         "slug": "mobile-accessories",
+//         "name": "Mobile Accessories",
+//         "url": "https://dummyjson.com/products/category/mobile-accessories"
+//     },
+//     {
+//         "slug": "motorcycle",
+//         "name": "Motorcycle",
+//         "url": "https://dummyjson.com/products/category/motorcycle"
+//     },
+//     {
+//         "slug": "skin-care",
+//         "name": "Skin Care",
+//         "url": "https://dummyjson.com/products/category/skin-care"
+//     },
+//     {
+//         "slug": "smartphones",
+//         "name": "Smartphones",
+//         "url": "https://dummyjson.com/products/category/smartphones"
+//     },
+//     {
+//         "slug": "sports-accessories",
+//         "name": "Sports Accessories",
+//         "url": "https://dummyjson.com/products/category/sports-accessories"
+//     },
+//     {
+//         "slug": "sunglasses",
+//         "name": "Sunglasses",
+//         "url": "https://dummyjson.com/products/category/sunglasses"
+//     },
+//     {
+//         "slug": "tablets",
+//         "name": "Tablets",
+//         "url": "https://dummyjson.com/products/category/tablets"
+//     },
+//     {
+//         "slug": "tops",
+//         "name": "Tops",
+//         "url": "https://dummyjson.com/products/category/tops"
+//     },
+//     {
+//         "slug": "vehicle",
+//         "name": "Vehicle",
+//         "url": "https://dummyjson.com/products/category/vehicle"
+//     },
+//     {
+//         "slug": "womens-bags",
+//         "name": "Womens Bags",
+//         "url": "https://dummyjson.com/products/category/womens-bags"
+//     },
+//     {
+//         "slug": "womens-dresses",
+//         "name": "Womens Dresses",
+//         "url": "https://dummyjson.com/products/category/womens-dresses"
+//     },
+//     {
+//         "slug": "womens-jewellery",
+//         "name": "Womens Jewellery",
+//         "url": "https://dummyjson.com/products/category/womens-jewellery"
+//     },
+//     {
+//         "slug": "womens-shoes",
+//         "name": "Womens Shoes",
+//         "url": "https://dummyjson.com/products/category/womens-shoes"
+//     },
+//     {
+//         "slug": "womens-watches",
+//         "name": "Womens Watches",
+//         "url": "https://dummyjson.com/products/category/womens-watches"
+//     }
+// ]
   // const Data = {
   //   Products: [
   //     {
@@ -223,6 +224,26 @@ const Dashboard = () => {
   //   "skip": 20,
   //   "limit": 10
   // }
+
+  useEffect(() => { 
+    setLoading(true)
+    fetch('https://dummyjson.com/products/categories')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`)
+        }
+        return res.json()
+      })
+      .then(data => {
+        setCategories(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error("Fetch error:", err)
+        setError({ type: "fetchError", message: "An unexpected error occurred." })
+        setLoading(false);
+      });
+  },[])
 
   useEffect(() => { 
     setLoading(true)
