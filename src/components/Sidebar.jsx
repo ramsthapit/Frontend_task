560f96be2b8ad004a769354e9791c72a096cbe29
Ram Sthapit <ramsthapit50@gmail.com>
2025-05-16 14:58:03 +0545

    feat(): react router added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,30 @@
 import React from 'react'
 import { FaTachometerAlt, FaShoppingCart, FaUsers, FaUser, FaBox, FaCog } from 'react-icons/fa';
+import { Link } from 'react-router';
 const Sidebar = () => {
+	const data = [
+		{ id: 1, name: 'Dashboard', icon: <FaTachometerAlt />, link: '/' },
+		{ id: 2, name: 'Data', icon: <FaShoppingCart />, link: '/Data' },
+		{ id: 3, name: 'Customers', icon: <FaUsers />, link: '/customers' },
+		{ id: 4, name: 'Users', icon: <FaUser />, link: '/users' },
+		{ id: 5, name: 'Products', icon: <FaBox />, link: '/products' },
+		{ id: 6, name: 'Settings', icon: <FaCog />, link: '/settings' },
+		
+	]
   return (
     <div className="flex flex-col w-full bg-gray-100 text-gray-900 h-full px-4 border-r border-gray-300 dark:border-gray-600 dark:bg-gray-900 dark:text-white">
 			<h1 className='text-2xl font-bold hidden md:block mt-4 text-center italic'>Ram Sthapit</h1>
 			<ul className='flex flex-col mt-5 text-xl'>
-				<li className='flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
-        hover:bg-blue-600 hover:text-white'>
-					<FaTachometerAlt />
-					<span className='hidden md:inline'>Dashboard</span>
-				</li>
-				<li className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
-        hover:text-white hover:bg-blue-600">
-					<FaShoppingCart />
-					<span className="hidden md:inline ">Orders</span>
-				</li>
-				<li className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
-        hover:text-white hover:bg-blue-600">
-					<FaUsers />
-					<span className="hidden md:inline ">Customers</span>
-				</li>
-				<li className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
-        hover:text-white hover:bg-blue-600">
-					<FaUser />
-					<span className="hidden md:inline ">Users</span>
-				</li>
-				<li className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
-        hover:text-white hover:bg-blue-600">
-					<FaBox />
-					<span className="hidden md:inline ">Products</span>
-				</li>
-				<li className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
-        hover:text-white hover:bg-blue-600">
-					<FaCog />
-					<span className="hidden md:inline ">Settings</span>
-				</li>
+				{data.map((item, index) => (
+					<Link to={item.link} key={index}>
+						<li className='flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
+						hover:bg-blue-600 hover:text-white'>
+							{item.icon}
+							<span className='hidden md:inline'>{item.name}</span>
+						</li>
+					</Link>
+				))}
+				
 			</ul>
 		</div>
   )
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Sidebar from './Sidebar'
-import Dashboard from '../pages/Dashboard'
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div className="flex flex-row w-full min-h-screen">
       <div className="flex w-1/4">
         <Sidebar /> 
       </div>
-      <div className="flex flex-col w-3/4 bg-black">
+      <div className="flex flex-col w-3/4">
         <Header />
-        <Dashboard />
+        {children}
       </div>
     </div>
   )
